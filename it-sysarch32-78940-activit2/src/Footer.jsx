de0c17a9ff5aa6f561c690df1750609fab654ec3
Rican19 <ricanmanguilimotan19@gmail.com>
2024-03-16 19:59:16 +0800

    adding something

---
+++ it-sysarch32-78940-activit2/src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer(){
+
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()}  IT-SYSARCH32-78940 Made by Rican Manguilimotan</p>
+        </footer>
+
+    );
+
+}
+export default Footer
--- it-sysarch32-78940-activit2/src/Header.jsx
+++ it-sysarch32-78940-activit2/src/Header.jsx
@@ -1,7 +1,17 @@
 function Header(){
 return(
+
     <header>
         <h1>Rican Manguilimotan</h1>
+      <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+            
+        </ul>
+      </nav>
     </header>
 
 );
